3eb8e38b62285f8d6896dd3eb80004c7d5e5eb4c
Ken Dickey <seatacken@gmail.com>
2020-11-23 15:23:16 -0800

    Working raycast and headbob fix

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        // from, direction, distance
+        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range); //returns bool, did you hit yes/no
+        Debug.Log("i hit this thing: " + hit.transform.name);
     }
 }
