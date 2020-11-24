e5cbf071e430f23246efebffec4f3f6c91baa420
Ken Dickey <seatacken@gmail.com>
2020-11-23 17:28:54 -0800

    Added vfx to bow and enemy with all tied to bow script

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,7 +8,9 @@ public class Weapon : MonoBehaviour
 
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
-
+    [SerializeField] float damage = 30f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -20,9 +22,38 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        // from, direction, distance
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range); //returns bool, did you hit yes/no
-        Debug.Log("i hit this thing: " + hit.transform.name);
+        // from, direction, info output I think, distance
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) //returns bool, did you hit yes/no
+        {
+            CreateHitImpact(hit);
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return; //if obj does not have EnemyHealth, do nothing
+
+            target.TakeDamage(damage); //tells EnemyHealth script to take damage when hit
+        }
+        else
+        {
+            return; //to protect from nullref errors
+        }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        //obj, location, direction(purpendicular to surface)
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 1); //destroy the fx
     }
 }
