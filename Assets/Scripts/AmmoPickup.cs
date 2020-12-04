f42667a731b31612b53e65eba0e4255be74c78ba
Ken Dickey <seatacken@gmail.com>
2020-12-03 20:30:18 -0800

    Ammo pickup creation and function

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") //any other obj with collider called player 
+        {
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+            Destroy(gameObject);
+        }
+    }
+}
